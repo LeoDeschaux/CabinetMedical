d674acab922044aa061f27176f48be070a32aa97
LeoDeschaux <leo.db40@gmail.com>
2021-11-27 21:26:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Accueil</title>
+</head>
+<body>
+
+<h1>Cabinet Medical</h1>
+
+<iframe src="https://giphy.com/embed/jpbnoe3UIa8TU8LM13" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+</body>
+</html>
