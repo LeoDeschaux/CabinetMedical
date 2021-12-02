11516c0b508150987392c2bec62ab819f4e28fc2
LeoDeschaux <leo.db40@gmail.com>
2021-12-02 20:20:04 +0100

    001

--- scripts/header.php
+++ scripts/header.php
@@ -5,28 +5,54 @@ if($var == '1') {
 
 $path = '/www/CabinetMedical/';
 //echo $path; 
+
+$stylePage1 = "menu";
+$stylePage2 = "menu";
+$stylePage3 = "menu";
+$stylePage4 = "menu";
+$stylePage5 = "menu";
+
+$currentPageName = basename($_SERVER['PHP_SELF']);
+
+if($currentPageName == "accueil.php")
+	$stylePage1 = "menu-current";
+
+if($currentPageName == "usagers.php" || $currentPageName == "ajouter.php" ||
+   $currentPageName == "modifier.php" || $currentPageName == "rechercher.php" ||
+   $currentPageName == "supprimer.php")
+	$stylePage2 = "menu-current";
+
+if($currentPageName == "medecins.php")
+	$stylePage3 = "menu-current";
+
+if($currentPageName == "planning.php")
+	$stylePage4 = "menu-current";
+
+if($currentPageName == "statistiques.php")
+	$stylePage5 = "menu-current";
+
 ?>
 
 <nav>
 	<div class="table">
 	<ul>
-		<li class="menu">
+		<li class="<?php echo $stylePage1;?>">
 			<a class ="accueil" href="<?php echo $path.'pages/accueil.php';?>">Accueil</a>
 		</li>
 
-		<li class="menu">
+		<li class="<?php echo $stylePage2;?>">
 			<a class ="usagers" href="<?php echo $path.'pages/usagers.php';?>">Usagers</a>
 		</li>
 		
-		<li class="menu">
+		<li class="<?php echo $stylePage3;?>">
 			<a class ="medecins" href="<?php echo $path.'pages/medecins.php';?>">Medecins</a>
 		</li>
 
-		<li class="menu">
+		<li class="<?php echo $stylePage4;?>">
 			<a class ="planning" href="<?php echo $path.'pages/planning.php';?>">Planning</a>
 		</li>
 		
-		<li class="menu">
+		<li class="<?php echo $stylePage5;?>">
 			<a class ="statistiques" href="<?php echo $path.'pages/statistiques.php';?>">Statistiques</a>
 		</li>
 		
@@ -39,63 +65,4 @@ $path = '/www/CabinetMedical/';
 
 <?php
 }
-else{
 ?>
-
-<?php }
-?>
-
-<?php
-	switch ($CouleurMenu) {
-		case 'Accueil':
-			?>
-			<style type="text/css"> 
-				.accueil {
-					background-color: rgba(155, 160, 200, 0.50);
-				}
-			</style>
-			<?php
-			break;
-		
-		case 'Planning':
-			?>
-			<style type="text/css"> 
-				.planning {
-					background-color: rgba(155, 160, 200, 0.50);
-				}
-			</style>
-			<?php
-			break;
-		
-		case 'Statistiques':
-			?>
-			<style type="text/css"> 
-				.statistiques {
-					background-color: rgba(155, 160, 200, 0.50);
-				}
-			</style>
-			<?php
-			break;
-		
-		case 'Usagers':
-			?>
-			<style type="text/css"> 
-				.usagers {
-					background-color: rgba(155, 160, 200, 0.50);
-				}
-			</style>
-			<?php
-			break;
-		
-		case 'Médecins':
-			?>
-			<style type="text/css"> 
-				.medecins {
-					background-color: rgba(155, 160, 200, 0.50);
-				}
-			</style>
-			<?php
-			break;
-	}
-?>
-
