d88e6b8bb957c677dabeb11a826439e882dd4322
LeoDeschaux <leo.db40@gmail.com>
2021-12-18 08:11:14 +0100

    017 - Ajout Medecin referent sur rechercher usager.php

--- pages/usagers/rechercher.php
+++ pages/usagers/rechercher.php
@@ -28,54 +28,25 @@ include($_SERVER['DOCUMENT_ROOT'] . '/CabinetMedical/scripts/usagersMenu.php'); 
 
 		function onFieldChange($linkpdo)
 		{
-			if(empty($_POST['search'])) {
-				showAllUsagers($linkpdo);
-			} else {
-				showCorrespondingUsagers($linkpdo);
-			}
+			showUsagers($linkpdo);
 		}
 
-		function showCorrespondingUsagers($linkpdo) {
-		    $req = $linkpdo->prepare("SELECT * FROM usager WHERE nom=:nom OR prenom=:prenom OR adresse=:adresse OR cp=:cp OR ville=:ville");
+		function showUsagers($linkpdo) {
+
+			if(empty($_POST['search'])) {
+				$req = $linkpdo->query("SELECT * FROM usager");
+			} else {
+				$req = $linkpdo->prepare("SELECT * FROM usager WHERE nom LIKE :nom OR prenom LIKE :prenom OR adresse LIKE :adresse OR cp LIKE :cp OR ville LIKE :ville");
+			}
+
 		    $field = $_POST['search'];
 
 		    $req->execute(array(
-		    'nom' => $field,
-		    'prenom' => $field,
-		    'adresse' => $field,
-		    'cp' => $field,
-		    'ville' => $field));
-		    
-		    ///Affichage des entrées du résultat une à une
-		    echo "<h2>Liste de tous les contacts :</h2>";
-
-		    echo "<table class=\"tableau_table\">";
-		    echo "<tr class=\"tableau_cell_title\">";
-		        echo "<th>Nom</th>";
-		        echo "<th>Prenom</th>";
-		        echo "<th>Adresse</th>";
-		        echo "<th>CodePostal</th>";
-		        echo "<th>Ville</th>";
-		    echo "</tr>";
-
-		    while ($row = $req->fetch()) {
-		        echo "<tr class=\"tableau_cell_title\">";
-		            echo "<td class=\"tableau_cell\">" . $row['nom'] . "</td>";
-		            echo "<td class=\"tableau_cell\">" . $row['prenom'] . "</td>";
-		            echo "<td class=\"tableau_cell\">" . $row['adresse'] . "</td>";
-		            echo "<td class=\"tableau_cell\">" . $row['cp'] . "</td>";
-		            echo "<td class=\"tableau_cell\">" . $row['ville'] . "</td>";
-		        echo "</tr>";
-		    }
-
-		    echo "</table>";
-		    $req->closeCursor(); 
-		}
-
-
-		function showAllUsagers($linkpdo) {
-		    ///Sélection de tout le contenu de la table carnet_adresse
-		    $req = $linkpdo->query("SELECT * FROM usager");
+		    'nom' => $field . "%",
+		    'prenom' => $field . "%",
+		    'adresse' => $field . "%",
+		    'cp' => $field . "%",
+		    'ville' => $field . "%"));
 		    
 		    ///Affichage des entrées du résultat une à une
 		    echo "<h2>Liste de tous les contacts :</h2>";
@@ -95,12 +66,31 @@ include($_SERVER['DOCUMENT_ROOT'] . '/CabinetMedical/scripts/usagersMenu.php'); 
 		        echo "<th>Lieu de naissance</th>";
 		        echo "<th>Date de naissance</th>";
 
+		        echo "<th>Médecin Référent</th>";
+	        	echo "<th>Ajouter consultation</th>";
+
 		        echo "<th>Modifier</th>";
 		        echo "<th>Supprimer</th>";
 		    echo "</tr>";
 
+		    $medecin_referent = "null";
+
 		    while ($row = $req->fetch()) {
 		    	$id = $row['id_u'];
+
+		    	if(!empty($row['id_m']))
+		    	{
+			    	$req_medecin = $linkpdo->prepare("SELECT medecin.nom, medecin.prenom FROM medecin, usager WHERE medecin.id_m=:id_m");
+
+			    	$req_medecin->execute(array(
+			    		'id_m' => $row['id_m'] 
+			    	));
+
+			    	while($medecin_row = $req_medecin->fetch())
+			    	{
+			    		$medecin_referent = $medecin_row['nom'] . " " . $medecin_row['prenom']; 
+			    	}
+		    	}
 
 		        echo "<tr class=\"tableau_cell_title\">";
 		            echo "<td class=\"tableau_cell\">" . $row['nom'] . "</td>";
@@ -115,6 +105,10 @@ include($_SERVER['DOCUMENT_ROOT'] . '/CabinetMedical/scripts/usagersMenu.php'); 
 
 		            echo "<td class=\"tableau_cell\">" . $row['lieu_naissance'] . "</td>";
 		            echo "<td class=\"tableau_cell\">" . Date("d-m-Y", $row['date_naissance']). "</td>";
+
+		            echo "<td class=\"tableau_cell\">" . $medecin_referent . "</td>";
+
+            		echo "<td class=\"tableau_cell\"><a href=\"#\">Ajouter une consultation</a></td>";
 
 		            echo "<td class=\"tableau_cell\"><a href=\"modifier.php?id=$id\">Modifier</a></td>";
 	                echo "<td class=\"tableau_cell\"><a href=\"supprimer.php?id=$id\">Supprimer</a></td>";
