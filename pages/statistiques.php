1595d44f997cede6a9e704b2caf7eb83a130be65
SvcePvte <paul-alexandre.baudrand@etu.iut-tlse3.fr>
2021-12-21 16:12:22 +0100

    005

--- pages/statistiques.php
+++ pages/statistiques.php
@@ -7,37 +7,40 @@ include($_SERVER['DOCUMENT_ROOT'] . '/CabinetMedical/scripts/header.php');
 ?>
 <!DOCTYPE HTML>
 <html>
-	
 	<head>
 		<title>Statistiques</title>
     	<meta charset="utf-8" />
     	<link rel="stylesheet" href="../styles/defaut.css">
+    	<link rel="stylesheet" href="../styles/statistiques.css">
 	</head>
-
 	<body>
-
-		<!--
-		<img src="../images/golo.jpg">
-		-->
-		<section style="text-align: center">
-			<h1 style="font-family: arial;font-size: 5em;color: deeppink;, text-align: center !important;">
-				STATISTIQUES
-			</h1>
-			<table>
-				<thead>
-					<tr>
-						<th colspan="2">boubouboubou</th>
-					</tr>
-				</thead>
+		<table>
+			<thead>
+				<tr>
+					<th>Tranche d'âge</th>
+					<th>Nb Hommes</th>
+					<th>Nb Femmes</th>
+				</tr>
+			</thead>
+			<tfoot>
 				<tbody>
-			        <tr>
-			            <td>The table body</td>
-			            <td>with two columns</td>
-			        </tr>
-			    </tbody>
-			</table>
-		</section>
-		
+					<tr>
+						<td>Moins de 25 ans</td>
+						<td> - 25 ans Hommes</td>
+						<td> - 25 ans Femmes</td>
+					</tr>
+					<tr>
+						<td>Entre 25 et 50 ans</td>
+						<td>hommes</td>
+						<td>femmes</td>
+					</tr>
+					<tr>
+						<td>Plus de 50 ans</td>
+						<td> + 50 ans Hommes</td>
+						<td> + 50 ans Femmes</td>
+					</tr>
+				</tbody>
+			</tfoot>
+		</table>
 	</body>
-
 </html>
