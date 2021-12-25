b5594697dbd1e5e272d17c85c6a499995677b823
SvcePvte <paul-alexandre.baudrand@etu.iut-tlse3.fr>
2021-12-25 17:51:36 +0100

    024

--- scripts/footer.php
+++ scripts/footer.php
@@ -1,3 +1,4 @@
 <footer>
-	<div class="text-footer">DESCHAUX-BEAUME Léo & BAUDRAND Paul-Alexandre | M3104 Groupe D</div>
+	<div class="text-footer"> DESCHAUX-BEAUME Léo & BAUDRAND Paul-Alexandre | M3104 Groupe D </div>
+	<img class="logo" src="/CabinetMedical/images/logo.jpg"> 
 </footer>
